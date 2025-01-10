41ab2bf78ac1696261a74a0f205852cb51436c47
nhathout <nhathout@bu.edu>
2025-01-10 02:37:36 -0500

    BUGFIX was not listening for the reset button press

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.post('/api/leaderboard/reset', (req, res) => {
 });
 
 // Listen on some port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
